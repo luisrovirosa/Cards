1503f7465a13a5b89e1829d9622040c6cae819e8
Luis Rovirosa <luisrovirosa@gmail.com>
2014-04-23 22:14:25 +0200

    no winner with the same card

--- card.js
+++ card.js
@@ -10,9 +10,16 @@ require('chai').should();
 
 var cardRanking = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K'];
 var giveMeTheWinner = function(pack1, pack2) {
-  var winner = cardRanking.indexOf(pack1[0]) > cardRanking.indexOf(pack2[0]) ? 1 : 2;
+  var player1CardRank = cardRanking.indexOf(pack1[0]);
+  var player2CardRank = cardRanking.indexOf(pack2[0]);
+  if (player1CardRank > player2CardRank) {
+    return 'Player 1 wins 1 to 0';
+  } else if (player1CardRank < player2CardRank) {
+    return 'Player 2 wins 1 to 0';
+  } else {
+    return 'No winner';
+  }
 
-  return 'Player ' + winner + ' wins 1 to 0';
 };
 
 describe('Cards', function() {
@@ -21,5 +28,8 @@ describe('Cards', function() {
   });
   it('player 2 wins with a single card with non-numeric cards', function() {
     giveMeTheWinner(['Q'], ['K']).should.eql('Player 2 wins 1 to 0');
+  });
+  it('no winner with the same card', function() {
+    giveMeTheWinner(['Q'], ['Q']).should.eql('No winner');
   });
 });
