c7f07cad8b65cdfd59230c51687feab4d55ddb73
Luis Rovirosa <luisrovirosa@gmail.com>
2014-04-23 22:30:47 +0200

    player 2 wins with 2 cards

--- card.js
+++ card.js
@@ -12,10 +12,24 @@ var cardRanking = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K'];
 var giveMeTheWinner = function(pack1, pack2) {
   var player1CardRank = cardRanking.indexOf(pack1[0]);
   var player2CardRank = cardRanking.indexOf(pack2[0]);
+  var player1CardRank2 = cardRanking.indexOf(pack1[1]);
+  var player2CardRank2 = cardRanking.indexOf(pack2[1]);
+  var scorePlayer1 = 0, scorePlayer2 = 0;
   if (player1CardRank > player2CardRank) {
-    return 'Player 1 wins 1 to 0';
+    ++scorePlayer1;
   } else if (player1CardRank < player2CardRank) {
-    return 'Player 2 wins 1 to 0';
+    ++scorePlayer2;
+  }
+  if (player1CardRank2 > player2CardRank2) {
+    ++scorePlayer1;
+  } else if (player1CardRank2 < player2CardRank2) {
+    ++scorePlayer2;
+  }
+
+  if (scorePlayer1 > scorePlayer2) {
+    return 'Player 1 wins ' + scorePlayer1 + ' to ' + scorePlayer2;
+  } else if (player1CardRank < player2CardRank) {
+    return 'Player 2 wins ' + scorePlayer2 + ' to ' + scorePlayer1;
   } else {
     return 'No winner';
   }
@@ -31,5 +45,8 @@ describe('Cards', function() {
   });
   it('no winner with the same card', function() {
     giveMeTheWinner(['Q'], ['Q']).should.eql('No winner');
+  });
+  it('player 2 wins with 2 cards', function() {
+    giveMeTheWinner(['1', '2'], ['4', '5']).should.eql('Player 2 wins 2 to 0');
   });
 });
