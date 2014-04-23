122bef938860ddd10b43f04cdde226185e8fbf29
Luis Rovirosa <luisrovirosa@gmail.com>
2014-04-23 22:08:42 +0200

    player 2 wins with a single card with non-numeric cards

--- card.js
+++ card.js
@@ -8,15 +8,18 @@
 
 require('chai').should();
 
+var cardRanking = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K'];
 var giveMeTheWinner = function(pack1, pack2) {
-  var winner = pack1[0] > pack2[0] ? 1 : 2;
+  var winner = cardRanking.indexOf(pack1[0]) > cardRanking.indexOf(pack2[0]) ? 1 : 2;
 
   return 'Player ' + winner + ' wins 1 to 0';
 };
 
 describe('Cards', function() {
   it('player 2 wins with a single card', function() {
-
     giveMeTheWinner(['1'], ['2']).should.eql('Player 2 wins 1 to 0');
+  });
+  it('player 2 wins with a single card with non-numeric cards', function() {
+    giveMeTheWinner(['Q'], ['K']).should.eql('Player 2 wins 1 to 0');
   });
 });
